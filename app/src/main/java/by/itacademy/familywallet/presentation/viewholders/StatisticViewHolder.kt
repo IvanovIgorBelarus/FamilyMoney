6c3a89bbdbf89f63d9da76c2fe5fbd8ca4613101
IgorIvanov <Igor.Ivanov@alseda.by>
2021-03-19 13:33:39 +0300

    little fix

--- app/src/main/java/by/itacademy/familywallet/presentation/viewholders/StatisticViewHolder.kt
+++ app/src/main/java/by/itacademy/familywallet/presentation/viewholders/StatisticViewHolder.kt
@@ -1,5 +1,6 @@
 package by.itacademy.familywallet.presentation.viewholders
 
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import by.itacademy.familywallet.App
 import by.itacademy.familywallet.databinding.TypeRecyclerItemBinding
@@ -12,6 +13,7 @@ class StatisticViewHolder(
     fun bind(item: UIModel.StatisticModel?) {
         App().viewPreparation.prepareView(itemView, item?.type!!)
         with(binding.textView) {
+            gravity=TextView.TEXT_ALIGNMENT_TEXT_START
             text = String.format("%s : %.2f BYN", item?.category, item?.value)
             App().viewPreparation.prepareView(this, item?.type!!)
         }
